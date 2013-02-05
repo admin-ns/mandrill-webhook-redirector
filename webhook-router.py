9c94a7eb3a65281751d1d729691076879604b576
jake <jrobs585@gmail.com>
2013-02-04 18:44:10 -0700

    added a GET

--- webhook-router.py
+++ webhook-router.py
@@ -3,13 +3,15 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST', 'HEAD'])
+@app.route("/", methods=['POST','GET', 'HEAD'])
 def index():
     if request.method == 'HEAD':
         return ''
+    if request.method == 'GET':
+        return 'reqIest = GET'
     mandrill_event = json.loads(request.form['mandrill_events'])
     metadata = (mandrill_event[0]['msg']['metadata'])
     domain = metadata['domain']
     return redirect(domain.rstrip('/') + "/menu/autounsub/")
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
